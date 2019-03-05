7196b00d8c3d3121b200aea45a4a68f7601e7f13
Mitsuya.WATANABE <watanabe@gocco.co.jp>
2019-03-05 11:42:22 +0900

    Set canvas size to fit parent container

--- src/index.js
+++ src/index.js
@@ -4,32 +4,44 @@ import './style.scss';
 
 import Particles from './js/particles/Particles.js'
 
-const sketch = (p5) => {
-	// make library globally available
-	window.p5 = p5;
+window.onload = function () {
 
-  let particles = {}
-  let img = {};
-	p5.setup = () => {
-    const canvas = p5.createCanvas(500, 500)
-    const context = canvas.elt.getContext("2d");
-    canvas.parent("canvas");
+  // NOTE: Set canvas size.
+  //       This size is set in css. See style.scss.
+  const initialCanvas = document.getElementById('canvas');
+  const width = initialCanvas.clientWidth;
+  const height = initialCanvas.clientHeight;
 
-    // NOTE: Image Setting
-    p5.imageMode(p5.CENTER);
-    img = p5.loadImage('./assets/p5js.svg');    
-
-    // NOTE: Particles(Canvas)
-    const particleNum = 250;
-    particles = new Particles();
-    particles.createParticles(context, particleNum);
+  const sketch = (p5) => {
+    // make library globally available
+    window.p5 = p5;
+  
+    let particles = {}
+    let img = {};
+    p5.setup = () => {
+      const canvas = p5.createCanvas(width, height)
+      const context = canvas.elt.getContext('2d');
+      
+      // NOTE: Fit p5 canvas with parent container. 
+      //       Now p5 canvas will be set as fullscreen inside parent.
+      canvas.parent('canvas');
+  
+      // NOTE: Image Setting
+      p5.imageMode(p5.CENTER);
+      img = p5.loadImage('./assets/p5js.svg');    
+  
+      // NOTE: Particles(Canvas)
+      const particleNum = 250;
+      particles = new Particles();
+      particles.createParticles(context, particleNum);
+    }
+  
+    p5.draw = () => {
+      p5.background(255);
+      particles.render();
+      p5.image(img, p5.width / 2, p5.height / 2);
+    }
   }
-
-	p5.draw = () => {
-    p5.background(255);
-    particles.render();
-    p5.image(img, p5.width / 2, p5.height / 2);
-	}
+  
+  new p5(sketch);
 }
-
-new p5(sketch);
